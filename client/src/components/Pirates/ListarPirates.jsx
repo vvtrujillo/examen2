e7be65eaa08f20664b7326e8d13e791928cd7d8c
vvtrujillo <victor.valenzuela.t@gmail.com>
2022-08-31 13:31:33 -0400

    cambios menores

--- client/src/components/Pirates/ListarPirates.jsx
+++ client/src/components/Pirates/ListarPirates.jsx
@@ -12,10 +12,14 @@ const ListarPirates = ({datos, eliminarFn}) => {
                 {
                     datos.map((j,i) => 
                         <div className='content-pirate'>
-                            <img src={j.linkimagen}></img>
-                            <h1>{j.nombre}</h1>
-                            <Button color='primary'>Ver pirata</Button>
-                            <Button color='danger' onClick={e => eliminarFn(j)}>Eliminar pirata</Button>                            
+                            <div className='content-pirate-left'>
+                                <img src={j.linkimagen}></img>
+                            </div>
+                            <div className='content-pirate-right'>
+                                <h1>{j.nombre}</h1>
+                                <Button color='primary'>Ver pirata</Button>
+                                <Button color='danger' onClick={e => eliminarFn(j)}>Eliminar pirata</Button>
+                            </div>
                         </div>
                     )
                 }
